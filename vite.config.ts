e1df5026fa8ab9c33c4fdb1e2a605b1b093e5289
BerniDev <jbernabemarin@gmail.com>
2024-09-14 18:45:23 +0200

    fix: icon rouest in pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { open: true },
-  base: '/redlightgreenlight/',
+  // base: '/redlightgreenlight/',
   plugins: [
     react(),
     VitePWA({
@@ -18,12 +18,12 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "/192.png",
+            src: "192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/512.png",
+            src: "512.png",
             sizes: "512x512",
             type: "image/png",
           },
